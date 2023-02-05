a022509da558f628b6310e7823c1ae6d562ae4e9
sumit7617 <sumitgaikwad221515@gmail.com>
2023-02-06 03:13:19 +0530

    commit all
    all files added.

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      scale: {
+        '175': '2',
+      }
+    },
+  },
+  plugins: [],
+  fontFamily:{
+    Whitney:["Whitney"],
+    Ginto:["Ginto"],
+    ggSans:["ggSans"],
+  },
+}
